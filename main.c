216a25aa6882bf6d77205e19866bad60a4a564f0
MatiasCardullo <supermaty97@gmail.com>
2018-06-02 20:51:12 -0300

    floats
    -acepta numeros con coma

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main(){
     char* puntero;
     char input[100];
     puntero=&input[0];
-    int negativeFlag=0,numFlag=0;
-    int aux,numCounter=0;
-    float auxNumbers[50];
-    for(int i=0;i<=50;i++){auxNumbers[i]=0;}
+    int negativeFlag=0,numFlag=0,floatFlag=0;
+    int numCounter=0,floatConter=0;
+    float aux,auxNums[50];
+    for(int i=0;i<=50;i++){auxNums[i]=0;}
     printf("____________________________________________________________\n");
     printf("\tIngrese la operacion:\n\n ");
     gets(puntero);
@@ -20,19 +20,32 @@ int main(){
         aux=*(puntero+i)-48;
         if(*(puntero+i)=='-'){
             if(numFlag){
-                numCounter++;numFlag=0;
-            }if(negativeFlag){negativeFlag=0;}
-            else{negativeFlag=1;}
+                numCounter++;
+                numFlag=0;
+            }
+            if(negativeFlag)
+                {negativeFlag=0;}
+            else
+                {negativeFlag=1;}
         }else if(aux>=0&&aux<=9){
             //printf("numero %d\n",aux);
-            auxNumbers[numCounter]=auxNumbers[numCounter]*10+aux;
-            numFlag=1;
+            if(floatFlag){
+                auxNums[numCounter]=auxNums[numCounter]+aux/potencia(10,floatConter);
+                printf("%.3f",auxNums[numCounter]);
+                floatConter++;
+            }else{
+                auxNums[numCounter]=auxNums[numCounter]*10+aux;
+            }numFlag=1;
+        }else if(*(puntero+i)=='.'||*(puntero+i)==','){
+            printf("hola");
+            floatFlag=1;floatConter=1;
         }else{
             if(negativeFlag){
-                auxNumbers[numCounter]=0-auxNumbers[numCounter];
+                auxNums[numCounter]=0-auxNums[numCounter];
                 negativeFlag=0;
             }if(numFlag){
-                numCounter++;numFlag=0;
+                numCounter++;
+                numFlag=0;floatFlag=0;
             }
         }if(*(puntero+i)=='\0'){break;}
         //printf("char %c\n",*(puntero+i));
@@ -40,9 +53,9 @@ int main(){
     //system("pause");system("cls");
     float output=0;
     for(int i=0;i<=5;i++){
-        printf("\nNumero %d: %.2f\n",i+1,auxNumbers[i]);
-        output=output+auxNumbers[i];
-    }printf("El resultado es %.2f\n",output);
+        printf("\nNumero %d: %.3f\n",i+1,auxNums[i]);
+        output=output+auxNums[i];
+    }printf("El resultado es %.3f\n",output);
     printf("__________________________________________\n ");
     system("pause");
     return 0;
@@ -53,7 +66,6 @@ int potencia(int numero, int potencia){
     if(potencia>0){
         for(int i=0;i<potencia;i++){
             resultado=resultado*numero;
-            printf("%d",resultado);
         }
     }
     return resultado;
